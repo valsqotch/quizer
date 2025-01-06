e20ebf6645020c1665e386ce977cb57210dc115b
valsqotch <136604293+valsqotch@users.noreply.github.com>
2025-01-06 22:24:51 +0800

    Update quizer.js

--- quizer.js
+++ quizer.js
@@ -1,28 +1,37 @@
-const obj1=document.getElementById("obj1");
+const ipt=document.getElementById("ipt");
+const btn=document.getElementById("btn");
 console.log("running");
-obj1.addEventListener("click",function(){
-    this.innerText="event activated";
-})
 function callBackend(){
     fetch("https://ux9t3s9dgd.execute-api.us-east-1.amazonaws.com/quizer0",{
         method:'GET'
     })
         .then(response => response.json())
         .then(data =>{
-            obj1.innerText = data.body;
-        })   
-}
-function runFetch(){
-    fetch("https://hdyeovw90a.execute-api.us-east-1.amazonaws.com/runFetch",{
-        
-    })
-        .then(response => response.json())
-        .then(data =>{
-            obj1.innerText = data.body;
+            console.log(data);
         })   
 }
 document.addEventListener("keydown",function(info){
     if(info.key==' '){
-        callBackend();
+        //callBackend();
+        console.log("space pressed");
     }
+})
+btn.addEventListener("click",function(){
+    fetch("https://ux9t3s9dgd.execute-api.us-east-1.amazonaws.com/quizer0",{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            comm:ipt.value.trim()
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error =>{
+        console.log("error occured,", error);
+    })
+    ipt.value='';
 })
